1b0cc61977f3f7d086d782fd2c5713a4e25fd6a8
4a8g0128 <100751965+4a8g0128@users.noreply.github.com>
2022-12-13 23:46:35 +0800

    Add files via upload

--- AIRehabilitation-main/main.py
+++ AIRehabilitation-main/main.py
@@ -7,6 +7,8 @@ import cv2
 import csv
 import sys
 import os
+import math
+import time 
 import numpy as np
 import mediapipe as mp
 import qtmodern.styles
@@ -59,7 +61,7 @@ class FullBodyPoseEmbedder(object):
 
     # Normalize landmarks.
     landmarks = self._normalize_pose_landmarks(landmarks)
-
+    
     return landmarks
 
   def _normalize_pose_landmarks(self, landmarks):
@@ -115,9 +117,11 @@ class FullBodyPoseEmbedder(object):
     return max(torso_size * torso_size_multiplier, max_dist)
 
 class creatcsv():  
-   def __init__(self ,landmarks ,csvs_out_folder) :
+
+   def __init__(self ,landmarks ,csvs_out_folder,pose_name) :
         self.landmarks = landmarks
         self.csvs_out_folder = csvs_out_folder
+        self.pose_name=pose_name
         
    def creat(self) :
         
@@ -134,12 +138,114 @@ class creatcsv():
         csv_out_path = os.path.join(self.csvs_out_folder, "pose"+ '.csv')
         with open(csv_out_path, 'w') as csv_out_file:
             csv_out_writer = csv.writer(csv_out_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-            csv_out_writer.writerow(self.landmarks)
+            csv_out_writer.writerow([self.pose_name] + self.landmarks.flatten().astype(np.str).tolist())
 
+class PoseSample(object):
+
+  def __init__(self, name, landmarks, class_name, embedding):
+    self.name = name
+    self.landmarks = landmarks
+    self.class_name = class_name
+    
+    self.embedding = embedding
+
+class PoseSampleOutlier(object):
+
+  def __init__(self, sample, detected_class, all_classes):
+    self.sample = sample
+    self.detected_class = detected_class
+    self.all_classes = all_classes
       
-            
+class poencsv(object):
+    def __init__(self,pose_samples_folder,
+                fullbody,
+                file_extension='csv',
+                file_separator=',',
+                n_landmarks=33,
+                n_dimensions=3,
+                top_n_by_max_distance=30,
+                top_n_by_mean_distance=10, 
+                axes_weights=(1., 1., 0.2)):
+        self.full_boby = fullbody
+        self._n_landmarks = n_landmarks
+        self._n_dimensions = n_dimensions
+        self._top_n_by_max_distance = top_n_by_max_distance
+        self._top_n_by_mean_distance = top_n_by_mean_distance
+        self._axes_weights = axes_weights
+        self._pose_samples = self._load_pose_samples(pose_samples_folder,
+                                                 file_extension,
+                                                 file_separator,
+                                                 n_landmarks,
+                                                 n_dimensions,
+                                                 fullbody)
+    def _load_pose_samples(self,
+                         pose_samples_folder,
+                         file_extension,
+                         file_separator,
+                         n_landmarks,
+                         n_dimensions,
+                         fullbody):
+        file_names = [name for name in os.listdir(pose_samples_folder) if name.endswith(file_extension)]
 
-   
+        pose_samples = []
+        for file_name in file_names:
+            # Use file name as pose class name.
+            class_name = file_name[:-(len(file_extension) + 1)]
+      
+        # Parse CSV.
+        with open(os.path.join(pose_samples_folder, file_name)) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=file_separator)
+            for row in csv_reader:
+                assert len(row) == n_landmarks * n_dimensions + 1, 'Wrong number of values: {}'.format(len(row))
+                landmarks = np.array(row[1:], np.float32).reshape([n_landmarks, n_dimensions])
+                pose_samples.append(PoseSample(
+                    name=row[0],
+                    landmarks=landmarks,
+                    class_name=class_name,
+                    embedding=fullbody(landmarks),
+          ))
+
+        return pose_samples                                                 
+    def __cell__(self, pose_landmarks):
+        """Classifies given pose.
+        Classification is done in two stages:
+        * First we pick top-N samples by MAX distance. It allows to remove samples
+            that are almost the same as given pose, but has few joints bent in the
+            other direction.
+        * Then we pick top-N samples by MEAN distance. After outliers are removed
+            on a previous step, we can pick samples that are closes on average.
+        
+        Args:
+        pose_landmarks: NumPy array with 3D landmarks of shape (N, 3).
+        Returns:
+        Dictionary with count of nearest pose samples from the database. Sample:
+            {
+            'pushups_down': 8,
+            'pushups_up': 2,
+            }
+        """
+           
+
+        # Get given pose embedding.
+        pose_landmarks = self.full_boby(pose_landmarks)
+        flipped_pose_embedding = self.full_boby(pose_landmarks * np.array([-1, 1, 1]))
+
+        # Filter by max distance.
+        #
+        # That helps to remove outliers - poses that are almost the same as the
+        # given one, but has one joint bent into another direction and actually
+        # represnt a different pose class.
+        #max_dist_heap = []
+        for sample_idx, sample in enumerate(self._pose_samples):
+            max_dist = min(
+            np.max(np.abs(sample.embedding - pose_landmarks) * self._axes_weights),
+            np.max(np.abs(sample.embedding - flipped_pose_embedding) * self._axes_weights),
+        )
+
+        mean_dist = 100 -  round(max_dist)
+
+        return mean_dist
+  
 
 class MainApp(QWidget):
     #pose_landmarks檔案路徑
@@ -150,6 +256,15 @@ class MainApp(QWidget):
         self.video_size = QSize(320, 240)
         self.setup_ui()
         self.setup_camera()
+        self.fullbody =  FullBodyPoseEmbedder()
+        self.pose_open=poencsv(pose_samples_folder='/Users/xushuyu/Desktop/ARTS-3bd075fb70a33877a95eb5a95226871a9f237316/pos_csvs',
+                fullbody=self.fullbody)
+        t=time.time()
+        t2=time.localtime(t)
+        self.t3=time.mktime(t2)
+        self.t4=self.t3+30
+               
+                    
 
     def setup_ui(self):
         """Initialize widgets.
@@ -180,7 +295,6 @@ class MainApp(QWidget):
         self.timer = QTimer()
         self.timer.timeout.connect(self.display_video_stream)
         self.timer.start(30)
-
     def display_video_stream(self):
         """Read frame from camera and repaint QLabel widget.
         """
@@ -188,6 +302,15 @@ class MainApp(QWidget):
         # 啟用姿勢偵測
         with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
             ret, img = self.capture.read()
+            t=time.time()
+            t2=time.localtime(t)
+            t3=time.mktime(t2)
+            x=0
+            second=round(self.t4-t3)
+            if second is x :
+                print('Finish')
+                exit()
+
             if not ret:
                 print("Cannot receive frame")
                 return
@@ -204,20 +327,29 @@ class MainApp(QWidget):
             image = QImage(img, img.shape[1], img.shape[0],  img.strides[0], QImage.Format_RGB888)
             myScaledPixmap = QPixmap.fromImage(image).scaled( self.image_label.size(), Qt.KeepAspectRatio)
             self.image_label.setPixmap(myScaledPixmap)
-            #
-            self.landmarks_pose=FullBodyPoseEmbedder(results)
-            #
+            
+            # 取得landmarks
             pose_landmarks = results.pose_landmarks
             if pose_landmarks is not None:
                 frame_height, frame_width = img.shape[0], img.shape[1]
                 pose_landmarks = np.array([[lmk.x * frame_width, lmk.y * frame_height, lmk.z * frame_width]
                                         for lmk in pose_landmarks.landmark], dtype=np.float32)
                 assert pose_landmarks.shape == (33, 3), 'Unexpected landmarks shape: {}'.format(pose_landmarks.shape)
-            #
+           
+            #紀錄User_landmarks
             csvinputfolder ='landmarks_Save'
-            csv=creatcsv(landmarks = pose_landmarks ,csvs_out_folder=csvinputfolder)
+            posename='123'
+            csv=creatcsv(landmarks = pose_landmarks ,csvs_out_folder=csvinputfolder,pose_name=posename)
             csv.creat()
-            print(pose_landmarks[0])
+               
+            poselandmarks=self.pose_open.__cell__(pose_landmarks)
+            #349 350畫面輸出 260訓練資料位置 340姿勢名稱
+            print(second)
+            print(str(poselandmarks)+'%')
+
+
+           
+
 
            
        
